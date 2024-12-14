97976754eed0fe6950969c6dd7505881434dd456
thalesgmendes <thalesmendes551@gmail.com>
2024-12-14 20:40:14 -0300

    adicionando VMWriter

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -1,10 +1,9 @@
 package br.ufma.ecp;
 
-import javax.swing.text.Segment;
-
 import br.ufma.ecp.SymbolTable.Kind;
 import br.ufma.ecp.token.Token;
 import br.ufma.ecp.token.TokenType;
+
 
 public class Parser {
 
@@ -16,9 +15,12 @@ public class Parser {
      private StringBuilder xmlOutput = new StringBuilder();
      private String className;
      private SymbolTable symbolTable;
+     private VMWriter vmWriter;
  
      public Parser(byte[] input) {
          scan = new Scanner(input);
+         symbolTable = new SymbolTable();
+         vmWriter = new VMWriter();
          nextToken();
      }
  
@@ -61,33 +63,6 @@ public class Parser {
 
         printNonTerminal("/parameterList");
     }
-
-     void parseSubroutineBody(String functionName, TokenType subroutineType) {
-
-        printNonTerminal("subroutineBody");
-        expectPeek(TokenType.LBRACE);
-        while (peekTokenIs(TokenType.VAR)) {
-            parseVarDec();
-        }
-        var nlocals = symbolTable.varCount(Kind.VAR);
-
-        vmWriter.writeFunction(functionName, nlocals);
-
-        if (subroutineType == CONSTRUCTOR) {
-            vmWriter.writePush(Segment.CONST, symbolTable.varCount(Kind.FIELD));
-            vmWriter.writeCall("Memory.alloc", 1);
-            vmWriter.writePop(Segment.POINTER, 0);
-        }
-
-        if (subroutineType == METHOD) {
-            vmWriter.writePush(Segment.ARG, 0);
-            vmWriter.writePop(Segment.POINTER, 0);
-        }
-
-        parseStatements();
-        expectPeek(TokenType.RBRACE);
-        printNonTerminal("/subroutineBody");
-     }
 
      void parseTerm() {
         printNonTerminal("term");
---
+++ src/main/java/br/ufma/ecp/VMWriter.java
@@ -0,0 +1,60 @@
+package br.ufma.ecp;
+
+public class VMWriter {
+    private StringBuilder vmOutput = new StringBuilder();
+
+    enum Segment {
+        CONST("constant"),
+        ARG("argument"),
+        LOCAL("local"),
+        STATIC("static"),
+        THIS("this"),
+        THAT("that"),
+        POINTER("pointer"),
+        TEMP("temp");
+    
+        private final String value; // Alterado para "private"
+    
+        private Segment(String value) {
+            this.value = value;
+        }
+    
+        public String getValue() { // Getter para acessar o valor
+            return value;
+        }
+    };
+
+    enum Command {
+        ADD,
+        SUB,
+        NEG,
+        EQ,
+        GT,
+        LT,
+        AND,
+        OR,
+        NOT
+    };
+
+    public String vmOutput() {
+        return vmOutput.toString();
+    }
+
+    void writePush(Segment segment, int index) {
+        vmOutput.append(String.format("push %s %d\n", segment.getValue(), index));
+    }
+
+    void writePop(Segment segment, int index) {
+
+        vmOutput.append(String.format("pop %s %d\n", segment.getValue(), index));
+    }
+
+    void writeFunction(String name, int nLocals) {
+        vmOutput.append(String.format("function %s %d\n", name, nLocals));
+    }
+
+    void writeCall(String name, int nArgs) {
+        vmOutput.append(String.format("call %s %d\n", name, nArgs));
+    }
+
+}

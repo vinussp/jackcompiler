9a57cc9018cd350b600b7e8bc975d64b0dd59839
thalesgmendes <thalesmendes551@gmail.com>
2025-01-07 12:14:03 -0300

    atualizando partseReturn

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -284,8 +284,11 @@ public class Parser {
         expectPeek(RETURN);
         if (!peekTokenIs(SEMICOLON)) {
             parseExpression();
+        } else {
+            vmWriter.writePush(Segment.CONST, 0);
         }
         expectPeek(SEMICOLON);
+        vmWriter.writeReturn();
         printNonTerminal("/returnStatement");
     }
 
